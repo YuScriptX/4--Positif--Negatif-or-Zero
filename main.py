cd245a3a246244ab9e7ab053850277e5d6c2c95f
YuScriptX <yusuf.tiftik@outlook.com>
2025-11-02 14:33:16 +0100

    add negative number condition to check function

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ num = int(input("Enter a random number?: "))
 def check_of_number():
     if num > 0:
         return "Number is positif"
+    elif num < 0:
+        return "Number is negatif"
     
 print(check_of_number())
